163a402dd3487cb57887ab50b4f8eeda59a55f8a
Tomasz Dyszewski <tomasz.dyszewski@dynatrace.com>
2023-03-16 19:43:32 +0100

    lesson-2

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Planet from './Planet';
 
-const Planets = () => {
+const fetchPlanets = async () => {
+	const res = await fetch('https://swapi.dev/api/planets/');
+	return res.json();
+}
+
+const Planets = () => {	
+	const { data, status } = useQuery("planets", fetchPlanets);
+	console.log('data');
+	console.log(data);
+
 	return (
 		<div>
 		  <h2>Planets</h2>
-		</div>
+
+		  {status === 'loading' && (<div>Loading data...</div>)}
+
+		  {status === 'error' && (<div>Error fetching data</div>)}
+
+		  {status === 'success' && (
+			<div>
+		  	{ data.results.map(planet => <Planet key={planet.name} planet={planet}/>)}
+			</div>
+		  )}
+
+        </div>
 );
 }
 
